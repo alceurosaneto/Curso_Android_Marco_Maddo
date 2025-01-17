6bd335002ce2eb5e57072fae556f2da5e9935b42
Alceu Rosa Neto <neto.alceu@gmail.com>
2025-01-16 21:53:04 -0300

    Acesso a atributos e metodo toString()

--- AppListaCurso/app/src/main/java/devandroid/rosaneto/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/rosaneto/applistacurso/view/MainActivity.java
@@ -13,6 +13,9 @@ import devandroid.rosaneto.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
     Pessoa pessoa;
+    Pessoa outraPessoa;
+    String dadosPessoa;
+    String dadosOutraPessoa;
 
 
     @Override
@@ -27,6 +30,35 @@ public class MainActivity extends AppCompatActivity {
         });
 
         pessoa = new Pessoa();
+        pessoa.setPrimeiroNome("Alceu");
+        pessoa.setSobreNome("Rosa");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("19 996087124");
+
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("ze");
+        outraPessoa.setSobreNome("felipe");
+        outraPessoa.setCursoDesejado("java");
+        outraPessoa.setTelefoneContato("11 11111111111");
+
+        dadosPessoa = "Primeiro nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += "Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += "Curso Desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += "Telefone de Contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        dadosOutraPessoa = "Primeiro nome: ";
+        dadosOutraPessoa += pessoa.getPrimeiroNome();
+        dadosOutraPessoa += "Sobrenome: ";
+        dadosOutraPessoa += pessoa.getSobreNome();
+        dadosOutraPessoa += "Curso Desejado: ";
+        dadosOutraPessoa += pessoa.getCursoDesejado();
+        dadosOutraPessoa += "Telefone de Contato: ";
+        dadosOutraPessoa += pessoa.getTelefoneContato();
+
 
     }
 }
