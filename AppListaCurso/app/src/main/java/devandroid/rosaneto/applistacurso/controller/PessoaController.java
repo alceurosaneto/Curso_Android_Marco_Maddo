7fe21f5dee4dc13ee6a7886606e496772ea92f09
Alceu Rosa Neto <neto.alceu@gmail.com>
2025-01-18 13:15:25 -0300

    Criado o Controller Salvar e o Controller toString

--- AppListaCurso/app/src/main/java/devandroid/rosaneto/applistacurso/controller/PessoaController.java
+++ AppListaCurso/app/src/main/java/devandroid/rosaneto/applistacurso/controller/PessoaController.java
@@ -1,4 +1,23 @@
 package devandroid.rosaneto.applistacurso.controller;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import devandroid.rosaneto.applistacurso.model.Pessoa;
+
 public class PessoaController {
+
+
+    @NonNull
+    @Override
+    public String toString() {
+        Log.d("MVC_Controller","Controller Inicializada");
+        return super.toString();
+    }
+
+    public void salvar(Pessoa pessoa) {
+        Log.d("MVC_Controller","Salvo: "+pessoa.toString());
+
+    }
 }
